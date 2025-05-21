cb5547f1735a4ae1201f54a23eec63bed7565d9e
Santiago Prato <santiagoprato832@gmail.com>
2025-05-21 19:36:11 -0300

    table users

--- src/main/java/com/login/user/User.java
+++ src/main/java/com/login/user/User.java
@@ -15,12 +15,20 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @NotNull
+    @Column(nullable = false)
     private String username;
+
     @NotNull
+    @Column(nullable = false)
     private String password;
+
     @NotNull
+    @Column(nullable = false)
     private String email;
+
     @NotNull
+    @Column(nullable = false)
     private Integer age;
 }
